3180ee702225ae45dc3e05e82f06bd7363a2c365
Andy James <kana@ikimi.co>
2019-10-26 16:50:07 +0100

    Basic startuo configuration

--- ProjectFanta/Startup.cs
+++ ProjectFanta/Startup.cs
@@ -20,13 +20,11 @@ namespace ProjectFanta
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -35,22 +33,16 @@ namespace ProjectFanta
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseHttpsRedirection()
+                .Use(async (ctx, next) =>
+                {
+                    await next();
+                })
+                .UseStaticFiles();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
         }
     }
 }
